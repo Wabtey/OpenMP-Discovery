fc555e0b9899ca1c58f2a2fa6d10e048b3b3c6cd
Wabtey <wabtey@disroot.org>
2023-11-24 17:54:59 +0100

    format code

--- src/mapreduce-openmp.c
+++ src/mapreduce-openmp.c
@@ -10,34 +10,42 @@ int NB_WORDS = 10;
 int STRING_MAX_SIZE = 20;
 
 /* A function to generate random strings */
-void gen_random(char *s, int len) {
+void gen_random(char *s, int len)
+{
     static const char alphanum[] = "abcdefghijklmnopqrstuvwxyz";
-    for (int i = 0; i < len; ++i) {
+    for (int i = 0; i < len; ++i)
+    {
         s[i] = alphanum[rand() % (sizeof(alphanum) - 1)];
     }
     s[len] = 0;
 }
 
 /* A function to print arrays of strings */
-void printArrayStrings(char **array){
+void printArrayStrings(char **array)
+{
 
-    for(int i = 0; i < NB_WORDS; i++){
+    for (int i = 0; i < NB_WORDS; i++)
+    {
         printf("%s\n", array[i]);
     }
 }
 
 /* A function to print the counts */
-void printArrayInt(int *count){
+void printArrayInt(int *count)
+{
 
-    for(int i = 0; i < 26; i++){
+    for (int i = 0; i < 26; i++)
+    {
         printf("%d ", count[i]);
     }
 }
 
 /* A function that maps a String to a count of characters */
-void map1 (char* c, int *count){
+void map1(char *c, int *count)
+{
 
-    for(int i= 0; i < 26; i++){
+    for (int i = 0; i < 26; i++)
+    {
         count[i] = 0;
     }
     // to be completed
@@ -49,7 +57,8 @@ int main()
     // init
     char *array[NB_WORDS];
     srand(time(NULL));
-    for(int i = 0; i < NB_WORDS; i++){
+    for (int i = 0; i < NB_WORDS; i++)
+    {
         int strSize = 1 + rand() % STRING_MAX_SIZE;
         array[i] = (char *)malloc(strSize * sizeof(char));
         gen_random(array[i], strSize);
